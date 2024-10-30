a31a123130aa37c5564e3193607b4b4f2ebef061
denis moshkin <denis.moshkin.007@yandex.ru>
2024-10-30 12:50:18 +0300

    added collapse component

---
+++ src/ui-kit/collapse/collapse.test.ts
@@ -0,0 +1,38 @@
+import {mount} from '@vue/test-utils'
+import {describe, expect, it} from 'vitest'
+import collapse from './collapse.vue'
+import collapseItem from './collapseItem.vue'
+
+describe('button', () => {
+	it('closable', async () => {
+		const wrapper = mount(alert, {
+			props: {
+				closable: true
+			}
+		})
+
+		await wrapper.find('.btn-close').trigger('click')
+
+		expect(wrapper.emitted()).toHaveProperty('close')
+		expect(wrapper.emitted()).toHaveProperty('click')
+		expect(wrapper.emitted()).toHaveProperty('update:modelValue')
+		expect(wrapper.classes()).not.toContain('alert')
+	})
+
+	it('icon slot', async () => {
+		const wrapper = mount(alert, {
+			props: {
+				showIcon: false
+			}
+		})
+
+		expect(wrapper.element.querySelector('.alert-icon')).toBeFalsy()
+
+		await wrapper.setProps({
+			showIcon: true,
+			variant: 'success'
+		})
+
+		expect(wrapper.element.querySelector('.alert-icon')).toBeTruthy()
+	})
+})
---
+++ src/ui-kit/collapse/collapse.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="accordion accordion-flush">
+		<slot></slot>
+	</div>
+</template>
+
+<script lang="ts" setup>
+	import {provide} from 'vue'
+
+	const model = defineModel<string[]>({
+		default: []
+	})
+
+	const props = defineProps({
+		accordion: {
+			type: Boolean,
+			default: false
+		}
+	})
+
+	provide('collapse', {activeNames: model, parentProps: props})
+</script>
+
+<style lang="scss" scoped></style>
